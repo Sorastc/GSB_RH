74755c2bc2e58c23cd4eace29aafee5b4ceecce6
harich-s <swennharich2801@gmail.com>
2026-03-16 11:58:23 +0100

    Projet entier

---
+++ src/DAO/RegionDAO.java
@@ -0,0 +1,31 @@
+package DAO;
+
+import POJO.Region;
+
+public class RegionDAO extends DAO<Region> {
+
+	@Override
+	public boolean create(Region obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean delete(Region obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean update(Region obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Region find(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
