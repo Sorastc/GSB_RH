e9fa976624ad365a36e8d8f285f1c0d318ca15d9
harich-s <swennharich2801@gmail.com>
2026-03-18 13:31:19 +0100

    InterfaceMaJ

---
+++ src/Interface/InterfaceConnexion.java
@@ -0,0 +1,33 @@
+package Interface;
+
+import java.util.Scanner;
+
+import DAO.UtilisateurDAO;
+
+public class InterfaceConnexion {
+	
+	public static void Connexion() {
+		Scanner sc = new Scanner(System.in);
+        UtilisateurDAO userDAO = new UtilisateurDAO();
+
+        System.out.print("Login : ");
+        String login = sc.nextLine();
+
+        System.out.print("Mot de passe : ");
+        String mdp = sc.nextLine();
+
+        String mdpEnBase = userDAO.recupMdpByLogin(login);
+        
+
+        if (mdpEnBase != null && mdpEnBase.equals(mdp)) {
+            System.out.println("Bienvenue " + login);
+        } else {
+            System.out.println("Login ou mot de passe incorrect.");
+        }
+        sc.close();
+	}
+	public static void ConsulterUtilisateur() {
+		
+	}
+	
+}
