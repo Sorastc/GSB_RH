e48d28a1d932385e5c074be0be534b94add5e6b9
harich-s <swennharich2801@gmail.com>
2026-03-18 13:30:37 +0100

    Merge branch 'master' of https://github.com/Sorastc/GSB_RH.git

--- src/GSB_RH/Program.java
+++ src/GSB_RH/Program.java
@@ -6,24 +6,6 @@ import DAO.UtilisateurDAO;
 
 public class Program {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-        UtilisateurDAO userDAO = new UtilisateurDAO();
-
-        System.out.print("Login : ");
-        String login = sc.nextLine();
-
-        System.out.print("Mot de passe : ");
-        String mdp = sc.nextLine();
-
-        String mdpEnBase = userDAO.recupMdpByLogin(login);
-        
-
-        if (mdpEnBase != null && mdpEnBase.equals(mdp)) {
-            System.out.println("Bienvenue " + login);
-        } else {
-            System.out.println("Login ou mot de passe incorrect.");
-        }
-
-        sc.close();
+		
     }
 }
