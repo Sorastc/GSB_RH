84d90a81a79a03278452657ee6b650885843a554
harich-s <swennharich2801@gmail.com>
2026-03-27 15:42:16 +0100

    Progint

---
+++ src/GSB_RH/progInt.java
@@ -0,0 +1,13 @@
+package GSB_RH;
+
+import Interface.InterfaceConnexion;
+
+public class progInt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		InterfaceConnexion frame = new InterfaceConnexion();	
+		frame.setSize(700,500);
+		frame.setVisible(true);
+	}
+}
